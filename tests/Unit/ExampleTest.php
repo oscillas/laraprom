66c86a83beb7bbae9fc4149f0e5ad342bf927d3a
Jacob Ray Gilman <jacob@oscillas.com>
2025-02-10 15:33:14 -0600

    Create basic test to confirm test suite runs

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function does_the_test_suite_work(): void
+    {
+        $this->assertTrue(true);
+    }
+}
