53067318bc71492b92cc3762270640379150c70e
Jacob Ray Gilman <jacob@oscillas.com>
2025-02-14 15:57:03 -0600

    Switch DatadogMonitoringHelper interface to Event and MetricReporters

--- src/Helpers/DatadogMonitoringHelper.php
+++ src/Helpers/DatadogMonitoringHelper.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Oscillas\Laraprom\Helpers;
 
 use GuzzleHttp\Client;
+use Oscillas\Laraprom\Reporters\EventReporterInterface;
+use Oscillas\Laraprom\Reporters\MetricReporterInterface;
 
-class DatadogMonitoringHelper implements ApplicationMonitoringHelperInterface
+class DatadogMonitoringHelper implements EventReporterInterface, MetricReporterInterface
 {
     public function __construct(protected Client $guzzleClient)
     {
