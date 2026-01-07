24227ad4ef8d20a24a2417c5411fadc1b3a355db
Jaianna Brooks <jaianna.brooks@oscillas.com>
2026-01-07 17:16:29 -0600

    Update package config file with comments explaining options to configure

--- config/application_monitoring.php
+++ config/application_monitoring.php
@@ -1,9 +1,37 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Event Reporter Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the event reporter driver used to send events.
+    | Supported drivers: "datadog", "void"
+    |
+    */
     'events' => env('LARAPROM_EVENT_REPORTER', 'datadog'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Metric Reporter Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the metric reporter driver used to send metrics.
+    | Supported drivers: "cloudwatch", "datadog", "prometheus", "void"
+    |
+    */
     'metrics' => env('LARAPROM_METRIC_REPORTER', 'datadog'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Driver Configurations
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the settings for each reporter driver.
+    | The "void" driver does not require any configuration.
+    |
+    */
     'drivers' => [
         'cloudwatch' => [
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
